e4106b48ad5d2858a1d81fcbe8bf192188ef040e
diaa <diaa2442000@gmail.com>
2024-04-17 14:34:43 +0200

    handle buttombar

--- threads-clone/components/shared/Bottombar.tsx
+++ threads-clone/components/shared/Bottombar.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Bottombar() {
+    const pathname = usePathname();
+
     return (
-        <div>Bottombar</div>
+        <section className="bottombar">
+            <div className="bottombar_container">
+            {sidebarLinks.map((link) => {
+                    const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+                    return (
+                        <Link
+                            href={link.route}
+                            key={link.label} 
+                            className={`bottombar_link ${isActive && 'bg-primary-500'}`}
+                        >
+                            <Image
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={24}
+                                height={24}
+                            />
+                            <p className="text-light-1 max-sm:hidden text-subtle-medium">
+                                {link.label.split(/\s+/)[0]}
+                            </p>
+                        </Link>
+                    )
+                })}
+            </div>
+        </section>
     )
 }
--- threads-clone/components/shared/LeftSidebar.tsx
+++ threads-clone/components/shared/LeftSidebar.tsx
@@ -24,7 +24,7 @@ export default function LeftSidebar() {
                         <Link
                             href={link.route}
                             key={link.label}
-                            className={`leftsidebar_link ${isActive && 'bg-white/20'}`}
+                            className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
                         >
                             <Image
                                 src={link.imgURL}
